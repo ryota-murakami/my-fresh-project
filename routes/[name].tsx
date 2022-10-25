9cd6ac7fc4affd61f666f945a62c8c2c4c9312fe
ryota-murakami <dojce1048@gmail.com>
2022-10-25 23:43:29 +0900

    add fetch

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "preact/hooks";
 import { PageProps } from "$fresh/server.ts";
 
 export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+  const [joke, setJoke] = useState(null);
+  useEffect(() => {
+    fetch("/api/joke").then((response) => response.json()).then((data) => {
+      setJoke(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>Hello {props.params.name}</p>
+      <p>{joke ? joke : "loading..."}</p>
+    </div>
+  );
 }
